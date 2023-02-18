5a01b8f56021c0b7db23605abd67f8ecfec9558c
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2023-02-18 11:48:59 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+import { ResponsiveNavbar } from "react-hamburger-menus";
+import "react-hamburger-menus/dist/style.css";
+
+export const Default = () => (
+  <ResponsiveNavbar
+    className='responsive-navbar'
+    logo={<p className='bukhgolts'>BUKHGOLTS</p>}
+    styles={{
+      navigation: { fontFamily: 'Arial, Helvetica, sans-serif', color: 'black' },
+      navigationBarSmall: {
+        paddingTop: "1em",
+        backgroundColor: 'white',
+      },
+      navigationBarLarge: {
+        paddingTop: "1em",
+        backgroundColor: 'white',
+      },
+      navigationCardSmall: {
+        backgroundColor: 'white',
+      },
+    }}
+  >
+      <ul>
+        <li>ABOUT</li>
+        <li>PROJECTS</li>
+        <li>CONTACT</li>
+      </ul>
+
+  </ResponsiveNavbar>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>!</div>
-      </header>
+
+      <Default />
+
+
     </div>
   );
 }
