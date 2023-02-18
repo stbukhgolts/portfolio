2d1f5b5394753e4872f71f3a2ac537f07cdb63b5
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2023-02-18 18:10:04 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import React from 'react';
-import happy from './happyo.jpg'
+import happy from './happyo.jpg';
 
-import { ResponsiveNavbar } from "react-hamburger-menus";
-import "react-hamburger-menus/dist/style.css";
+import { ResponsiveNavbar } from 'react-hamburger-menus';
+import 'react-hamburger-menus/dist/style.css';
+import { ImageViewer } from 'react-image-viewer-dv';
+import img1 from './1.png';
+import img2 from './2.png';
+import img3 from './3.png';
+
 
 export const Default = () => (
   <ResponsiveNavbar
-    className='responsive-navbar'
-    logo={<p className='bukhgolts'>BUKHGOLTS</p>}
+    className="responsive-navbar"
+    logo={<p className="bukhgolts">BUKHGOLTS</p>}
     styles={{
-      navigation: { fontFamily: 'Arial, Helvetica, sans-serif', color: 'black' },
+      navigation: {
+        fontFamily: 'Arial, Helvetica, sans-serif',
+        color: 'black',
+      },
       navigationBarSmall: {
-        paddingTop: "1em",
+        paddingTop: '1em',
         backgroundColor: 'white',
       },
       navigationBarLarge: {
-        width: "100%",
-        paddingTop: "1em",
+        width: '100%',
+        paddingTop: '1em',
         backgroundColor: 'white',
       },
       navigationCardSmall: {
@@ -25,22 +33,43 @@ export const Default = () => (
       },
     }}
   >
-      <ul>
-        <li>ABOUT</li>
-        <li>GALLERY</li>
-        <li>CONTACT</li>
-      </ul>
-
+    <ul>
+      <li>ABOUT</li>
+      <li>GALLERY</li>
+      <li>CONTACT</li>
+    </ul>
   </ResponsiveNavbar>
 );
+
+export const YourComponent = () => {
+  return (
+    <div className="gallery">
+      <h1>Image gallery</h1>
+
+      <div className="images">
+        <ImageViewer>
+          <img src={img1} alt="1" />
+        </ImageViewer>
+        <ImageViewer>
+          <img src={img2} alt="1" />
+        </ImageViewer>{' '}
+        <ImageViewer>
+          <img src={happy} alt="1" />
+        </ImageViewer>{' '}
+        <ImageViewer>
+          <img src={img3} alt="1" />
+        </ImageViewer>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-
       <Default />
-      <img src={happy} alt='happy old man' className='main-pic'/>
-
+      <img src={happy} alt="happy old man" className="main-pic" />
+      <YourComponent />
     </div>
   );
 }
